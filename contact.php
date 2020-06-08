5facf164094e36fcfe9d283dc454b2340163e65a
Raghav Gupta <65210035+rg436@users.noreply.github.com>
2020-06-08 19:16:58 +0530

    Add files via upload

--- contact.php
+++ contact.php
@@ -1,17 +1,18 @@
 <?php
 
 if($_POST["submit"]) {
-    $recipient="your@email.address";
-    $subject="Form to email message";
-    $sender=$_POST["sender"];
-    $senderEmail=$_POST["senderEmail"];
+    $recipient="ktmblanketsindia@gmail.com";
+    $subject="Query Alert";
+    $sender=$_POST["name"];
+    $senderEmail=$_POST["email"];
+    $senderCountry=$_POST["country"];
     $message=$_POST["message"];
 
-    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+    $mailBody="Name: $sender\nEmail: $senderEmail\nCountry: $senderCountry\n\n$message";
 
     mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>");
 
-    $thankYou="<p>Thank you! Your message has been sent.</p>";
+    $thankYou="<p>Thank you! Your message has been sent. </p>";
 }
 
 ?>
